18ed6d22bf450c88f520088ceb60cabf0ce9e6ac
Carlos Escobar <carlosWebDev11@gmail.com>
2023-10-24 11:24:15 -0700

    name-change

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Stats from "./StatsPage";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
